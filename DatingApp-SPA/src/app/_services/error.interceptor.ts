f16ebad398d39b9ce7b8642d3e40a848503496b7
hossein <hossein.sadeghikia@gmail.com>
2020-06-30 09:46:28 +0430

    End of Section 6

--- DatingApp-SPA/src/app/_services/error.interceptor.ts
+++ DatingApp-SPA/src/app/_services/error.interceptor.ts
@@ -19,7 +19,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
                     const serverError = error.error;
                     let modelStateErrors = '';
-                    if (serverError.error && typeof serverError.errors === 'object') {
+                    if (serverError.errors && typeof serverError.errors === 'object') {
                         for (const key in serverError.errors) {
                             if (serverError.errors[key]) {
                                 modelStateErrors += serverError.errors[key] + '\n';
